551d3c1a6cee7e3a763ac0ed038d67cc0dddae48
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-11-16 15:32:51 +0100

    Added header + search function

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
-  <div></div>
+  <header>
+    <HeaderComponent @filteredTitles="getTitles" />
+  </header>
 </template>
 
 <script>
 import { store } from "./store";
 import axios from "axios";
+import HeaderComponent from "./components/HeaderComponent.vue";
 
 export default {
+  components: { HeaderComponent },
   data() {
     return {
       store,
       endPoint: "search/movie",
       apiKey: "?api_key=843403d86580a9c697c8a3e94798c648",
+      apiQuery: "&query=",
     };
+  },
+  methods: {
+    getTitles() {
+      let options = null;
+      if (store.search.title) {
+        options = {
+          params: {
+            title: store.search.title,
+          },
+        };
+      }
+      const movieApi =
+        store.apiURL +
+        this.endPoint +
+        this.apiKey +
+        this.apiQuery +
+        this.store.search.title;
+      axios.get(movieApi, options).then((res) => {
+        store.movieList = res.data.results;
+        console.log(store.movieList);
+      });
+    },
   },
 };
 </script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="my-container d-flex justify-content-between align-items-center">
+      <div class="logo">BOOLFIX</div>
+
+      <div class="searchbar">
+        <input
+          type="text"
+          id="search-title"
+          placeholder="Search title..."
+          v-model.trim="store.search.title"
+        />
+        <button type="submit" @click="searchTitles">Search</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from "../store";
+
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    searchTitles() {
+      this.$emit("filteredTitles");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+.header {
+  height: 90px;
+  background-color: $headerblack;
+}
+
+.my-container {
+  height: 90px;
+}
+
+.logo {
+  color: red;
+}
+</style>
